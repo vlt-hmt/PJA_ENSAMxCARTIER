d9ee42932a174eb685b688d88c59048dbd775c21
vlt-hmt <valentin.hyaumet@ensam.eu>
2024-10-23 12:03:23 +0200

    Update log.js

--- log.js
+++ log.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Vérifier si l'utilisateur est connecté en vérifiant le localStorage
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-    if (!isLoggedIn) {
+    const isLoggedIn = localStorage.getItem('userToken');
+    if (!userToken) {
       // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
       window.location.href = 'index.html';
     }
